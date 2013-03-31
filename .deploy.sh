58bf13cc9b47e90e098b74444c3591ab582c8732
Lukas Bestle <lukas@lu-x.me>
2013-03-31 18:29:03 +0200

    Remove all that standard stuff

--- .deploy.sh
+++ .deploy.sh
@@ -1,16 +1,3 @@
-# Revert to last commit
-git clean -f .
-git checkout -- .
-
-# Fetch updates
-git pull
-git submodule init
-git submodule update
-
-# Update submodules
-git submodule foreach git checkout master
-git submodule foreach git pull
-
 # Compile stuff
 /var/www/virtual/plusplus/libs/sass/bin/sass --update assets/scss/main.scss:assets/css/main.css -f -t compressed
 /var/www/virtual/plusplus/libs/coffee/bin/coffee -c --output assets/js assets/coffee/*
