8de4b6fa15ada0d13c3536ed0775d98b2e386e4d
Lukas Bestle <lukas@lu-x.me>
2013-08-06 21:43:45 +0200

    Update to new logo

--- site/snippets/project.php
+++ site/snippets/project.php
@@ -7,7 +7,7 @@
 						$linkCloseTag = "";	
 					} ?>
 					<h2><?php echo $linkTag; ?><?php echo $p->title(); ?><?php echo $linkCloseTag; ?></h2>
-<?php $image = $p->images()->find('tile.jpg'); ?>
+<?php $image = $p->images()->find('tile.png'); ?>
 <?php if($image): ?>
 					<?php echo $linkTag; ?><img src="<?php echo $image->url(); ?>" height=200 width=200 alt="<?php echo $p->title(); ?>"><?php echo $linkCloseTag; ?>
 <?php endif; ?>
